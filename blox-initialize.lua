247d88d04613ed10184fdf60c046fb981a06e7f1
nilprovai <dqqd06@gmail.com>
2025-10-29 22:02:39 +0700

    Update blox-initialize.lua

--- blox-initialize.lua
+++ blox-initialize.lua
@@ -1974,7 +1974,7 @@ getgenv().IslandVariable.AllBoss = {
     },
     ["Sea 3"] = {
         "Stone","Island Empress","Kilo Admiral","Captain Elephant","Cake Queen","Beautiful Pirate",
-        "Soul Reaper","rip_indra True Form","Cake Prince","Dough King"
+        "Soul Reaper","rip_indra True Form","Cake Prince","Dough King", "Tyrant of the Skies"
     },
 }
 
