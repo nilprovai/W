83153895132a52497b99535f575e04745c040a43
nilprovai <dqqd06@gmail.com>
2025-11-01 00:28:57 +0700

    Update ui-loader.lua

--- ui-loader.lua
+++ ui-loader.lua
@@ -316,6 +316,7 @@ local UiIntilize = {
             {Mode = "Toggle",Title = "Ignore Ship",Id = "Ignore Ship"},
             {Mode = "Toggle",Title = "Auto Shark",Id = "Auto Shark"},
             {Mode = "Toggle",Title = "Auto Piranha",Id = "Auto Piranha"},
+            {Mode = "Toggle",Title = "Auto Fish Crew Member",Id = "Auto Fish Crew Member"},
             {Mode = "Toggle",Title = "Safe Mode",Id = "Safe Mode"},
             {Mode = "Toggle",Title = "Auto Escape Rough Sea",Id = "Auto Escape Rough Sea"},
         }},
